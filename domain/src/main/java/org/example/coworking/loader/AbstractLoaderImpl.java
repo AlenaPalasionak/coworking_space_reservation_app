f2021be956227fd4cc6f5418191a18c910b73fc5
Alena <19lenka87@tut.by>
2025-03-17 21:00:27 +0300

    52. Add functionality to AbstractLoaderImpl

--- domain/src/main/java/org/example/coworking/loader/AbstractLoaderImpl.java
+++ domain/src/main/java/org/example/coworking/loader/AbstractLoaderImpl.java
@@ -3,15 +3,13 @@ package org.example.coworking.loader;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.List;
-
-import static org.example.coworking.logger.Log.USER_OUTPUT_LOGGER;
-import static org.example.coworking.logger.Log.TECHNICAL_LOGGER;
 
 /**
  * An abstract implementation of the {@link Loader} interface, providing common functionality
@@ -24,61 +22,65 @@ import static org.example.coworking.logger.Log.TECHNICAL_LOGGER;
  * @param <T> the type of the data to be loaded and saved
  */
 public abstract class AbstractLoaderImpl<T> implements Loader<T> {
+    protected static final Logger USER_OUTPUT_LOGGER = LoggerFactory.getLogger("UserOutputLogger");
+    protected static final Logger TECHNICAL_LOGGER = LoggerFactory.getLogger("TechnicalLogger");
 
-    /**
-     * An instance of {@link ObjectMapper} used for converting between Java objects and JSON.
-     */
-    protected ObjectMapper objectMapper;
+    protected final ObjectMapper objectMapper;
 
     public AbstractLoaderImpl() {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
     }
 
-    /**
-     * Abstract method to retrieve the file path where the data is stored.
-     * <p>
-     * This method must be implemented by subclasses to return the specific file path.
-     * </p>
-     *
-     * @return the file path where data is stored
-     */
     protected abstract String getFilepath();
 
-    @Override
-    public List<T> load(Class<T> beanType) throws FileNotFoundException {
+    /**
+     * Loads JSON-file and deserializes it into a list of objects.
+     */
+    public List<T> load(Class<T> beanType) {
         JavaType javaType = objectMapper.getTypeFactory().constructParametricType(List.class, beanType);
-        List<T> list = new ArrayList<>();
-        File jsonFile = new File(getFilepath());
-        if (!jsonFile.exists()) {
-            String message = "File with the name: " + getFilepath() + " is not found";
-            USER_OUTPUT_LOGGER.error(message);
-            TECHNICAL_LOGGER.error(message);
-            throw new FileNotFoundException(message);
+        List<T> list;
+
+        try (InputStream inputStream = loadResourceAsStream()) {
+            list = objectMapper.readValue(inputStream, javaType);
+        } catch (IOException e) {
+            String errorMessage = "Failed to parse JSON file: " + beanType.getName();
+            USER_OUTPUT_LOGGER.error(errorMessage, e);
+            TECHNICAL_LOGGER.error(errorMessage, e);
+            throw new RuntimeException("Error reading JSON", e);
         }
-        if (jsonFile.length() == 0) {
-            USER_OUTPUT_LOGGER.warn("JSON file " + jsonFile + " is empty");
-            TECHNICAL_LOGGER.warn("JSON file " + jsonFile + " is empty");
-        } else {
-            try {
-                list = objectMapper.readValue(jsonFile, javaType);
-            } catch (IOException e) {
-                USER_OUTPUT_LOGGER.error("Failed to parse JSON file:  " + beanType.getName() + "\n", e);
-                TECHNICAL_LOGGER.error("Failed to parse JSON file:  " + beanType.getName() + "\n", e);
-                throw new RuntimeException("Error reading JSON", e);
-            }
-        }
+
         return list;
     }
 
-    @Override
+    /**
+     * Loads resources as InputStream.
+     *
+     * @return InputStream for JSON-file
+     * @throws RuntimeException file is not found
+     */
+
     public void save(List<T> objects) {
+        File jsonFile = new File(getFilepath());
+
         try {
-            objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(getFilepath()), objects);
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(jsonFile, objects);
         } catch (IOException e) {
-            USER_OUTPUT_LOGGER.error(" Failed to write JSON file: " + getFilepath(), e);
-            TECHNICAL_LOGGER.error(" Failed to write JSON file: " + getFilepath(), e);
+            String errorMessage = "Failed to write JSON file: " + getFilepath();
+            USER_OUTPUT_LOGGER.error(errorMessage, e);
+            TECHNICAL_LOGGER.error(errorMessage, e);
             throw new RuntimeException("Error writing JSON", e);
         }
+    }
+
+    private InputStream loadResourceAsStream() {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(getFilepath());
+        if (inputStream == null) {
+            String message = "File not found in classpath: " + getFilepath();
+            USER_OUTPUT_LOGGER.error(message);
+            TECHNICAL_LOGGER.error(message);
+            throw new RuntimeException(message);
+        }
+        return inputStream;
     }
 }
